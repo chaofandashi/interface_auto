1d40fb329bdb755c470b10fe7112ffe71bda4f68
huangjunping <275769643@qq.com>
2018-03-02 18:30:08 +0800

    啥也没

--- test/ppp2.py
+++ test/ppp2.py
@@ -6,17 +6,29 @@ from selenium import webdriver
 import unittest
 import time
 import re
+import json
+import urllib.parse
 #禁用安全警告
-# urllib3.disable_warnings()
+urllib3.disable_warnings()
 
-url="https://www.baidu.com"
+url="https://passport.baidu.com/v2/api/?login"
 header={
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36",
-    "Cookie":"_ga=GA1.2.1621354742.1519443840; _gid=GA1.2.1031460666.1519443840; __utma=59123430.1621354742.1519443840.1519452779.1519452779.1; __utmc=59123430; __utmz=59123430.1519452779.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmt=1; __utmb=59123430.4.10.1519452779"
+    "Cookie":"SAVEUSERID=e0402cfe5a87f0c66329e480f38f5579574500dd; UBI=fi_PncwhpxZ%7ETaKAbCz9C49D2DNUSWgTOnYW%7ECiGJiIX-KrVsbrzZ74DO4IzHqe1U5-RcA3QbbYSDhX5ATk; USERNAMETYPE=2; HOSUPPORT=1; HISTORY=e33ac23a106d4251aa0a82c20ca811a957ae1a7eab55df65; FP_UID=e5b1aa48826e7d24b0c639b14d9f715c; BAIDUID=E1D73F42C31B6A34693C83A242B81AD3:FG=1; BIDUPSID=E1D73F42C31B6A34693C83A242B81AD3; PSTM=1519377877; cflag=15%3A3; BDRCVFR[k2U9xfnuVt6]=mk3SLVN4HKm; H_PS_PSSID=25843_1422_21126_22160"
 }
-r=requests.get(url)
+s=requests.session()
+r=s.get(url,headers=header,verify=False)
 
-print (r.status_code)
-print (r.headers)
-print (r.text)
+c = requests.cookies.RequestsCookieJar()
+c.set("BAIDUID",'E1D73F42C31B6A34693C83A242B81AD3:FG=1')
+c.set('BIDUPSID','E1D73F42C31B6A34693C83A242B81AD3')
 
+s.cookies.update(c)
+
+
+import re
+
+s=r'abc'
+c='aasddasdaasdabcbxcabc'
+c=re.findall(s,c)
+print (c)
